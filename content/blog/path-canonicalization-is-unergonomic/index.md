cc0a8ccbca399fad8c901582e3cb35faa5abf825
Simon Guest <simon.guest@tesujimath.org>
2025-01-18 14:51:36 +1300

    Early version

---
+++ content/blog/path-canonicalization-is-unergonomic/index.md
@@ -0,0 +1,13 @@
++++
+title = "Path canonicalization is unergonomic"
++++
+
+# Why?
+By far the biggest reason: Breakage in parent
+Using Parent in path probably means your code is broken. It should probably be deprecated.
+
+# Ergonomics
+Nix, stow, symlinks as an abstraction, the benefit of relative paths.
+
+# An alternative
+Real parent
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flake for building O my Soul";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          nativeBuildInputs = [
+            zola
+            just
+            static-web-server
+          ];
+        };
+      }
+    );
+}
